01f9b2a0804a7283e8622d4aac2fe64fecb28024
Shreya4net <pujakumari.civil@gmail.com>
2017-04-20 18:03:27 +0530

    android session6 assignment2
    android session6 assignment2

---
+++ app/src/main/java/com/dce/puja/listfragment/Layout1.java
@@ -0,0 +1,23 @@
+package com.dce.puja.listfragment;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Devendra on 4/20/2017.
+ */
+
+public class Layout1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+// TODO Auto-generated method stub
+        return  inflater.inflate(R.layout.layout1, container, false);
+
+    }
+}
+
+
+
